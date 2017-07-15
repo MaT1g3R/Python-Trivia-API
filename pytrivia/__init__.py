a12450f1fc321bfd4f8f485d8bc1cbdf6e758d36
Umi <982341777@qq.com>
2017-07-15 06:02:52 -0400

    fix setup for real

--- pytrivia/__init__.py
+++ pytrivia/__init__.py
@@ -3,7 +3,7 @@ from .trivia import Trivia
 
 __author__ = 'MaT1g3R'
 __title__ = 'Python-Trivia-API'
-__version__ = '4.0.1'
+__version__ = '4.0.2'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2017 MaT1g3R'
 
